c18a0eee8a860e46e32790a79f4bbce51915f914
Joseph Gaspar <josephg21886@gmail.com>
2016-07-26 04:28:22 -0400

    KDTree

--- KDTree/KDTree.lua
+++ KDTree/KDTree.lua
@@ -5,23 +5,36 @@ local M_M = {
 }
 local O_M = {}
 local M = {}
-	M.new = function(point, item)
+	M.new = function(point, item, depth)
 		local self = setmetatable({}, O_M)
-
 		self.point = point
 		self.item = item
-
+		self.depth = depth or 0
 		-- self.left
 		-- self.right
-
 		return self
 	end
 local O_I = {}
-	O_I.insert = function(point, item)
+	O_I.insert = function(self, point, item)
+		local i = (depth % #self.point) + 1
+
+		if self.point[i] > point[i] then
+			if self.left then
+				self.left:insert(point, item)
+			else
+				self.left = M(point, item, self.depth+1)
+			end
+		else
+			if self.right then
+				self.right:insert(point, item)
+			else
+				self.right = M(point, item, self.depth+1)
+			end
+		end
 	end
-	O_I.delete_at = function(point)
+	O_I.delete = function(self, point)
 	end
-	O_I.nearest_pals = function(point, radius)
+	O_I.range_query = function(self, range)
 		
 	end
 
